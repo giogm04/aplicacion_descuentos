0a4772e09279114c9bf0bd9fe5e8123c2eb1dc5a
giogm04 <71354676+giogm04@users.noreply.github.com>
2022-04-19 23:09:23 -0500

    version final

---
+++ descuentos.js
@@ -0,0 +1,18 @@
+function calcularPrecioConDescuento(precio, descuento) {
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+    return precioConDescuento;
+}
+
+function onClickButtonPrecioDescuento(){
+
+    const inputPrecio = document.getElementById("InputPrecio").value;
+
+    const inputDescuento = document.getElementById("InputDescuento").value;
+
+    const precioConDescuento = calcularPrecioConDescuento(inputPrecio, inputDescuento);
+
+    document.getElementById("ResultadoP").innerText = "Precio con descuento: $" + precioConDescuento;
+    
+}
